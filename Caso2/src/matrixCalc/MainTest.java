47ca094a6b92acdb7f59b2b582ac1bb4a38e79b2
samuelfreire03 <s.freire@uniandes.edu.co>
2023-04-04 19:17:31 -0500

    cambios en main y T

--- Caso2/src/matrixCalc/MainTest.java
+++ Caso2/src/matrixCalc/MainTest.java
@@ -1,9 +1,23 @@
 package matrixCalc;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MainTest {
 	
 	public static AgeCounter ageCounter = new AgeCounter();
 	
 	public static void main(String[] args) {
-	}
+        
+        PageTable paginas = new PageTable();
+
+        paginas.CrearPaginas(8, 8, 4, 256);
+
+        ArrayTable tabla = paginas.getTablaPagina();
+        Map<String, int[]> mapa = tabla.getTable();
+        Map<String, int[]> sortedMap = new TreeMap<>(mapa);
+        for (java.util.Map.Entry<String, int[]> entry : sortedMap.entrySet()) {
+            System.out.println(entry.getKey() + " tiene " + entry.getValue()[0] + "," + + entry.getValue()[1]);
+          }
+    }
 }
--- Caso2/src/matrixCalc/T.java
+++ Caso2/src/matrixCalc/T.java
@@ -7,18 +7,6 @@ import java.util.TreeMap;
 
 public class T{
 
-    public static void main(String[] args) {
-        
-        PageTable paginas = new PageTable();
-
-        paginas.CrearPaginas(8, 8, 4, 256);
-
-        ArrayTable tabla = paginas.getTablaPagina();
-        Map<String, int[]> mapa = tabla.getTable();
-        Map<String, int[]> sortedMap = new TreeMap<>(mapa);
-        for (java.util.Map.Entry<String, int[]> entry : sortedMap.entrySet()) {
-            System.out.println(entry.getKey() + " tiene " + entry.getValue()[0] + "," + + entry.getValue()[1]);
-          }
-    }
+    
     
 }
